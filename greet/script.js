77cb62d6b48fbd932537e78adf1eebfdad462196
Nejwa <aprime3463@gmail.com>
2025-10-06 21:18:51 -0700

    Keep only 'week 9' directory; remove other folders from repo

--- greet/script.js
+++
@@ -1,6 +0,0 @@
-let userName = prompt( "Hi! What's your name?" ) //ask user for it's name
-if (userName){
-    alert("Hello, " + userName + "! Welcome!");//if the user enters the name will be greeted
-}else{
-    alert("Hello, guest! Welcome!")
-}
